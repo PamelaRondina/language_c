da41d3411cc4b3f6ebd0c4afe4dd71400978c0e0
Pamela Rondina <pamela.rondina@gmal.com>
2025-02-27 22:00:41 +0000

    ajuste desagio nivel heroi

--- dio/01_gft_logica/02_nivel_heroi/nivel_heroi.c
+++ dio/01_gft_logica/02_nivel_heroi/nivel_heroi.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
     const char *heroi = "Ezequiel";
     char *rank;
-    int xp = 10001; // 6710 xp atual Pam (Dio) 27/02.
+    int xp = 6710; // 6710 xp atual Pam (Dio) 27/02.
 
-
-    if (xp <= 1000)
-        rank = "Ferro";
-    else if (xp <= 2000)
-        rank = "Bronze";
-    else if (xp <= 5000)
-        rank = "Prata";
-    else if (xp <= 7000)
-        rank = "Ouro";
-    else if (xp <= 8000)
-        rank = "Platina";
-    else if (xp <= 9000)
-        rank = "Ascendente";
-    else if (xp <= 10000)
-        rank = "Imortal";
-    else
-        rank = "Radiante";
-    printf("O Herói de nome %s está no nível %s\n", heroi, rank);
-
+        while (xp >= 0)
+        {
+            if (xp <= 1000)
+                rank = "Ferro";
+            else if (xp <= 2000)
+                rank = "Bronze";
+            else if (xp <= 5000)
+                rank = "Prata";
+            else if (xp <= 7000)
+                rank = "Ouro";
+            else if (xp <= 8000)
+                rank = "Platina";
+            else if (xp <= 9000)
+                rank = "Ascendente";
+            else if (xp <= 10000)
+                rank = "Imortal";
+            else
+                rank = "Radiante";
+            printf("O Herói de nome %s está no nível %s\n", heroi, rank);
+            return (0);
+        }
     return (0);
 }
 
-    /*char de string funciona pois recebe um ponteiro para salvar o nome do herói, 
+    /*
+    ok - Variáveis (int e char);
+    ok - Operadores permitem operações matemáticas, relacionais e lógicas (<=);
+    ok - Laços de repetição (while);
+    ok - Estruturas de decisão (if-else);
+
+    char de string funciona pois recebe um ponteiro para salvar o nome do herói, 
     somente leitura, string imutável, não deve/pode ser modificada.
     Caso queira modificar: char heroi[] = "Nome"
     
