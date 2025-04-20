dfae890d7eb1b284dd96d6d4d79e69166356c7d4
Pamela Rondina <pamela.rondina@gmail.com>
2025-04-20 22:27:13 +0100

    up libft

--- study/libft/libft.h
+++ study/libft/libft.h
@@ -9,6 +9,8 @@
 #include <ctype.h>
 #include <string.h>
 
+#include <stdlib.h>
+
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
@@ -25,7 +27,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_strnstr(const char *big,	const char *little, size_t len);
-
+int ft_atoi(const char *nptr);
  
 
 
