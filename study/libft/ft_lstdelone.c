cdd2e3275714d80163e3981b44ef1d38a0406da8
Pamela Rondina <pamela.rondina@gmail.com>
2025-05-03 15:51:26 +0100

    up bonus

--- study/libft/ft_lstdelone.c
+++ study/libft/ft_lstdelone.c
@@ -19,7 +19,39 @@ void ft_lstdelone(t_list *lst, void (*del)(void*))
     }
 }
 
+int main (void)
+{
+    t_list *node1;
+    t_list *node2;
+    t_list *list;
+    
+    node1 = ft_lstnew("Continue a ");
+    node2 = ft_lstnew("Nadar!");
+
+    node1->next = node2;
+    list = node1;
+
+    while(list) //Antes de funcao de deletar!
+    {       
+        printf("Antes de função: %s\n", (char *)list->content);
+        list = list->next;
+    }
+    ft_lstdelone(node2, del);
+    node1->next = NULL;
+
+    while(list) //Depois da função
+    {       
+        printf("Depois de função: %s\n", (char *)list->content);
+        list = list->next;
+    }
+
+
+
+
+    
+}
+
 //evitar vazamento na memoria
-        //1 - delera o conteudo da caixa    (del(node->content));
+        //1 - deleta o conteudo da caixa    (del(node->content));
         //2 - deleta a caixa                free(node);
         //(node, del) - dentro da funcao ja informa o content
